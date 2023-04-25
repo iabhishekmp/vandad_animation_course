48f52cf8e4b89628c2d3a8ef8439bb5450fdb4d4
Abhi <iabhishekmp@gmail.com>
2023-04-25 17:12:23 +0530

    Custom Painter & polygon

--- lib/example_7_custom_painter_and_polygon.dart
+++ lib/example_7_custom_painter_and_polygon.dart
@@ -2,8 +2,73 @@ import 'dart:math' show pi, cos, sin;
 
 import 'package:flutter/material.dart';
 
-class Example7 extends StatelessWidget {
+class Example7 extends StatefulWidget {
   const Example7({super.key});
+
+  @override
+  State<Example7> createState() => _Example7State();
+}
+
+class _Example7State extends State<Example7> with TickerProviderStateMixin {
+  late final AnimationController sideAnimationController;
+  late final Animation<int> sideAnimation;
+
+  late final AnimationController radiusAnimationController;
+  late final Animation<double> radiusAnimation;
+
+  late final AnimationController rotationAnimationController;
+  late final Animation<double> rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    sideAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    sideAnimation = IntTween(
+      begin: 3,
+      end: 10,
+    ).animate(sideAnimationController);
+
+    radiusAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    radiusAnimation = Tween<double>(
+      begin: 20,
+      end: 400,
+    )
+        .chain(CurveTween(curve: Curves.bounceInOut))
+        .animate(radiusAnimationController);
+
+    rotationAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 2 * pi,
+    )
+        .chain(CurveTween(curve: Curves.easeInOut))
+        .animate(rotationAnimationController);
+  }
+
+  @override
+  void dispose() {
+    sideAnimationController.dispose();
+    radiusAnimationController.dispose();
+    rotationAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    sideAnimationController.repeat(reverse: true);
+    radiusAnimationController.repeat(reverse: true);
+    rotationAnimationController.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +77,28 @@ class Example7 extends StatelessWidget {
         title: const Text('Example7'),
       ),
       body: Center(
-        child: SizedBox(
-          height: 500,
-          width: 500,
-          child: CustomPaint(
-            painter: Polygon(sides: 5),
-          ),
+        child: AnimatedBuilder(
+          animation: Listenable.merge([
+            sideAnimationController,
+            radiusAnimationController,
+            rotationAnimationController,
+          ]),
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateX(rotationAnimation.value)
+                ..rotateY(rotationAnimation.value)
+                ..rotateZ(rotationAnimation.value),
+              child: CustomPaint(
+                painter: Polygon(sides: sideAnimation.value),
+                child: SizedBox(
+                  height: radiusAnimation.value,
+                  width: radiusAnimation.value,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
